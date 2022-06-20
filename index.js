b696bc221be710b16a4b87a1b146d569080c6e92
Jas Mann <jaforshort@hotmail.ca>
2022-06-20 14:53:21 +0000

    Function that takes in an IP address and returns lat and long coords

--- index.js
+++ index.js
@@ -8,3 +8,14 @@
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('24.150.121.76', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
--- iss.js
+++ iss.js
@@ -22,4 +22,32 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+/**
+* Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+* Input:
+*   - The ip (ipv4) address (string)
+*   - A callback (to pass back an error or the lat/lng object)
+* Returns (via Callback):
+*   - An error, if any (nullable)
+*   - The lat and lng as an object (null if error). Example:
+*     { latitude: '49.27670', longitude: '-123.13000' }
+*/
+const fetchCoordsByIP = function(ip, callback) {
+ request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+   if (error) {
+     callback(error, null);
+     return;
+   }
+
+   if (response.statusCode !== 200) {
+     callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+     return;
+   }
+
+   const { latitude, longitude } = JSON.parse(body);
+
+   callback(null, { latitude, longitude });
+ });
+};
+
+module.exports = { fetchCoordsByIP };
